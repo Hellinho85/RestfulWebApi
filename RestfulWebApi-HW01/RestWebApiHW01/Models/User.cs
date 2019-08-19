062449bb16204eb0f2e8d330d59921c12e76a3e6
Gjorgji Kongulovski <46351282+Hellinho85@users.noreply.github.com>
2019-08-19 22:06:10 +0200

    RestfulWebApi01
    RestfulWebApi-HWRepo01

---
+++ RestfulWebApi-HW01/RestWebApiHW01/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWebApiHW01.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public int Id { get; set; }
+    }
+}
